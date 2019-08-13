3545884a23795f81cc89bd8823d8eb397e6dff4b
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-13 16:37:33 -0400

    finished explorer mode

--- public/main.js
+++ public/main.js
@@ -1,23 +1,29 @@
-const cityName = document.querySelector('.search-box').value
-const appID = '&appid=e8314de613adbb0263525f4baa035bd1'
+const url = 'https://api.openweathermap.org/data/2.5/weather?'
+const unitFormat = 'units=imperial'
+const appID = 'appid=e8314de613adbb0263525f4baa035bd1'
+let cityZip = 'q'
 
-const main = async () => {
+const getWeather = async () => {
+  const textInput = document.querySelector('.search-box').value
+  if (parseInt(textInput)) {
+    cityZip = 'zip'
+  } else {
+    cityZip = 'q'
+  }
   // call the API using fetch
   // eslint-disable-next-line no-undef
   const response = await fetch(
-    'http://api.openweathermap.org/data/2.5/weather?q='
+    `${url}${cityZip}=${textInput}&${unitFormat}&${appID}`
   )
   console.log(response)
   if (response.status === 200) {
     const weather = await response.json()
     console.log(weather)
-    document.querySelector('.display-city-name').textContent =
-      ' ' && weather.name
+    document.querySelector('.display-city-name').textContent = weather.name
     document.querySelector('.display-temp').textContent = weather.main.temp
     document.querySelector('.display-rain').textContent =
       weather.weather[0].description
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
-document.querySelector('.search-button').addEventListener('click', main)
+document.querySelector('.search-button').addEventListener('click', getWeather)
