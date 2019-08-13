f862c9ba69996ab718a71ce7cf3ea4b0f26b83e5
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-13 15:34:06 -0400

    working on concatanating the api

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const cityName = document.querySelector('.search-box').value
+const appID = '&appid=e8314de613adbb0263525f4baa035bd1'
+
+const main = async () => {
+  // call the API using fetch
+  // eslint-disable-next-line no-undef
+  const response = await fetch(
+    'http://api.openweathermap.org/data/2.5/weather?q='
+  )
+  console.log(response)
+  if (response.status === 200) {
+    const weather = await response.json()
+    console.log(weather)
+    document.querySelector('.display-city-name').textContent =
+      ' ' && weather.name
+    document.querySelector('.display-temp').textContent = weather.main.temp
+    document.querySelector('.display-rain').textContent =
+      weather.weather[0].description
   }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.search-button').addEventListener('click', main)
